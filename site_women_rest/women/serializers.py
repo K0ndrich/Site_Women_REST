8b7adb7fdcf55f58c0abb649c405729ed1b4ef35
K0ndrich <123kondrich@gmail.com>
2024-04-21 19:00:18 +0300

    edited

--- site_women_rest/women/serializers.py
+++ site_women_rest/women/serializers.py
@@ -1,13 +1,47 @@
 # В етом файле храняться сериализаторы для наших представлений в views.py
 from .models import Women, Category
 from rest_framework import serializers
+from rest_framework.renderers import JSONRenderer
+
+
+# простое создание обьекта
+class WomenModel:
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
+
+
+# создание сериализатора для модели Women
+# текущий сериализатор проверяет значения которые отправил клиент в POST запросе с указаными характеристиками
+class WomenSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+
+
+# сериализация обьекта - функция преобразовывает созданные обьеткы класса WomenModel в json формат
+
+def encode():
+    model = WomenModel("Sana", "OlEGOV")
+    # model_sr хранит внери себя обьект сериализации
+    model_sr = WomenSerializer(model)
+    # возвращает просто словарь со значениями   {'title': 'Sana', 'content': 'OlEGOV'}
+    print(model_sr.data, type(model_sr.data), sep="\n")
+    # переводим обьект сериализации в json строку
+    json = JSONRenderer().render(model_sr.data)
+    # возвращает байтовую строку json , которую можна отдавать пользователю   b'{"title":"Sana","content":"OlEGOV"}'
+    print(json)
+
+
+# десериализация обьекта - преобразование json строки в обьект класса WomenModel
+def decode():
+    pass
 
 
 # ModelSerializer работает только с нашими моделями
-class WomenSerializer(serializers.ModelSerializer):
-    class Meta:
-        # связываемся с нашей моделью Women
-        model = Women
-        # указываем поля нашей модели, которые попадут в сериализацию и будут отправляються обратно пользователю
-        # ети поля будут отображаться при сахождении в API сайта
-        fields = ("title", "cat")
+# class WomenSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         # связываемся с нашей моделью Women
+#         model = Women
+#         # указываем поля нашей модели, которые попадут в сериализацию и будут отправляються обратно пользователю
+#         # ети поля будут отображаться при сахождении в API сайта
+#         fields = ("title", "cat")
--- site_women_rest/women/views.py
+++ site_women_rest/women/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.forms import model_to_dict
 from .models import Women, Category
 from .serializers import WomenSerializer
 
@@ -12,13 +13,27 @@ from rest_framework.response import Response
 class WomenAPIView(APIView):
     # для обработки GET запросов
     def get(self, request):
+        # .values() позволяет брать значения, а не queryset
+        lst = Women.objects.all().values()
         # Response просто возвращает фиксированую указаную .json строку
-        return Response({"title": "my_value"})
+        return Response({"post": list(lst)})
+
+    # для обработки POST запросов
+    def post(self, request):
+        # создание новой записи в модели клиентов через POST запрос
+        post_new = Women.objects.create(
+            # requet.data["title"] - ето значения в URL адресе
+            title=request.data["title"],
+            content=request.data["content"],
+            cat_id=request.data["cat_id"],
+        )
+        # model_to_dict преобразовует запись или записи модели в тип словаря
+        return Response({"title": model_to_dict(post_new)})
 
 
 # ListAPIView отображает указаные записи из нашей модели, которую связали в serializers.py
 # class WomenAPIView(generics.ListAPIView):
 #     # queryset тоже самое что и get_query_set в простом django
-#     queryset = Women.objects.filter(is_published=True)
+#     queryset = Women.objects.all()
 #     # serializer_class указывает на сериализатор для текущего представления, который создали ниже
 #     serializer_class = WomenSerializer
